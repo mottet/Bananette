c5c5cb299065754411ca3cd1609d90abcb386aa2
Marvin Mottet <marvin@mottet.pro>
2018-08-20 23:35:03 +0200

    Add remove at the end of video

--- remove-recommandation.js
+++ remove-recommandation.js
@@ -3,7 +3,9 @@
 var forbiddenChannel = ["Solfa", "BuzzFeedVideo"];
 
 var configObserver = { attributes: true, childList: true, subtree: true };
-var nodeToObserve = document.getElementById('related');
+
+var nodeToObserveOnSide = document.getElementById('related');
+var nodeToObserveOnVideo = document.getElementsByClassName("html5-endscreen ytp-player-content videowall-endscreen")[0];
 
 var removeRecommandation = () => {
   var recommandation = document.getElementsByTagName('ytd-compact-video-renderer');
@@ -13,5 +15,16 @@ var removeRecommandation = () => {
   }
 };
 
+var removeRecommandationEndVideo = () => {
+  var recommandation = document.getElementsByClassName("ytp-videowall-still ytp-suggestion-set")
+  for (var i = 0; i < recommandation.length; ++i) {
+    if (forbiddenChannel.includes(recommandation[i].innerText.split('\n').reverse()[1].split('•')[0].trim()))
+      recommandation[i].outerHTML = "";
+  }
+};
+
 var observer = new MutationObserver(removeRecommandation);
-observer.observe(nodeToObserve, configObserver);
+observer.observe(nodeToObserveOnSide, configObserver);
+
+var observerEndVideo = new MutationObserver(removeRecommandationEndVideo);
+observerEndVideo.observe(nodeToObserveOnVideo, configObserver);
